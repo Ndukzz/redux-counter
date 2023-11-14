ab1c216594356bea8496489f60641b364df94eb9
Ndukwe Ebuka <ebukaemannuel@gmail.com>
2023-11-14 03:44:02 +0000

    Provided the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from "react-redux"
+import { Provider } from "react-redux"  //THE PROVIDER
 
 
 import './index.css';
 import App from './App';
-import store from "./store/index.js"
+import store from "./store/index.js"  // THE REDUX STORE
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
